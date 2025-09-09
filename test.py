2de00014ea1d5ef17770897ff975494dfab82e73
jsbowen79 <jsbowen79@outlook.com>
2025-09-09 10:46:39 -0600

    End of week 1

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-book = "My favorite book"
-print (book)
+def sum_of_odd_numbers(end):
+    total = 0
+    for i in range(0, end):
+        if (i%2 == 1):
+            total += i
+    return total
+print (sum_of_odd_numbers(10))
